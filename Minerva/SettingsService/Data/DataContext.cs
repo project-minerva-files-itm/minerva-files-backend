2c2957f2f5a348fc079d6ba510ccdb5323dd2247
brayanmedinacardozo96 <64286356+brayanmedinacardozo96@users.noreply.github.com>
2024-09-19 09:03:25 -0500

    feat: Add Entity Company

---
+++ Minerva/SettingsService/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SettingsService.Data
+{
+    public class DataContext : DbContext
+    {
+
+    }
+}
---
+++ Minerva/SharedLibrary/Responses/ActionResponse.cs
@@ -0,0 +1,39 @@
+﻿namespace SharedLibrary.Responses
+{
+    public class ActionResponse<T>
+    {
+        public bool WasSuccess { get; private set; }
+        public string? Message { get; private set; }
+        public T? Result { get; private set; }
+
+        private ActionResponse() { }
+
+        public class Builder
+        {
+            private readonly ActionResponse<T> _response = new();
+
+            public Builder SetSuccess(bool wasSuccess)
+            {
+                _response.WasSuccess = wasSuccess;
+                return this;
+            }
+
+            public Builder SetMessage(string message)
+            {
+                _response.Message = message;
+                return this;
+            }
+
+            public Builder SetResult(T result)
+            {
+                _response.Result = result;
+                return this;
+            }
+
+            public ActionResponse<T> Build()
+            {
+                return _response;
+            }
+        }
+    }
+}
