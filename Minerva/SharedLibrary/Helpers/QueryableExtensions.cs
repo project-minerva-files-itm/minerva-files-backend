5f6495cd7b2a3a00f8f9b8e62fc73a8574d74af1
brayanmedinacardozo96 <64286356+brayanmedinacardozo96@users.noreply.github.com>
2024-09-28 23:01:40 -0500

    feat: filtering

--- Minerva/SharedLibrary/Helpers/QueryableExtensions.cs
+++ Minerva/SharedLibrary/Helpers/QueryableExtensions.cs
@@ -1,4 +1,7 @@
-﻿using SharedLibrary.DTOs;
+﻿using Newtonsoft.Json;
+using SharedLibrary.DTOs;
+using System.Linq;
+using System.Linq.Expressions;
 
 
 namespace SharedLibrary.Helpers
@@ -10,6 +13,78 @@ namespace SharedLibrary.Helpers
             return queryable
                 .Skip((pagination.Page - 1) * pagination.Records)
                 .Take(pagination.Records);
+        }
+
+
+
+
+        public static IQueryable<T> GetFilteredDataAsync<T>(this IQueryable<T> queryable,string jsonString)
+        {
+            var filters = JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+
+            // Recorre los filtros y crea condiciones dinámicas
+            foreach (var filter in filters)
+            {
+                var propertyName = filter.Key;
+                var propertyValue = filter.Value;
+                // Construye la expresión de filtro
+                queryable = queryable.ApplyDynamicFilter(propertyName, propertyValue);
+            }
+
+            return queryable;
+        }
+
+        /// <summary>
+        /// Crea un método genérico para aplicar el filtro dinámico
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="propertyName"></param>
+        /// <param name="propertyValue"></param>
+        /// <returns></returns>
+        private static IQueryable<T> ApplyDynamicFilter<T>(this IQueryable<T> query, string propertyName, object propertyValue)
+        {
+            // Obtén el tipo de la entidad
+            var parameter = Expression.Parameter(typeof(T), "e");
+
+            // Crea la expresión para la propiedad: e.PropertyName
+            var property = Expression.Property(parameter, propertyName);
+
+            // Crea la expresión para el valor del filtro
+            var constant = Expression.Constant(propertyValue);
+
+        
+
+
+            if (propertyValue is string stringValue)
+            {
+                // Aquí aplicamos un "LIKE" que sería similar a SQL "%valor%"
+                var method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+
+                var likeExpression = Expression.Call(property, method, Expression.Constant(stringValue));
+
+                // Crea el lambda: e => e.PropertyName LIKE propertyValue
+                var lambda = Expression.Lambda<Func<T, bool>>(likeExpression, parameter);
+
+                // Aplica el filtro a la consulta
+                return query.Where(lambda);
+
+            }
+            else {
+
+                // Crea la comparación e.PropertyName == propertyValue
+                var equalExpression = Expression.Equal(property, constant);
+
+                // Crea el lambda: e => e.PropertyName == propertyValue
+                var lambda = Expression.Lambda<Func<T, bool>>(equalExpression, parameter);
+
+                // Aplica el filtro a la consulta
+                return query.Where(lambda);
+
+            }
+
+
+          
         }
     }
 }
