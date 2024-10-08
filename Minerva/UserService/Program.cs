2260f9a98e2dc34947ef6ebc21b8a664b5f4ef19
victorgalvis <victorgalvis.85@gmail.com>
2024-10-08 01:49:58 -0500

    feat: creation repositories and unitsofwork

--- Minerva/UserService/Program.cs
+++ Minerva/UserService/Program.cs
@@ -1,18 +1,25 @@
+using Fantasy.Backend.Repositories.Implementations;
+using Fantasy.Backend.UnitsOfWork.Implementations;
+using Microsoft.AspNetCore.Identity;
 using SharedLibrary.Data;
+using SharedLibrary.Entities;
+using System.Text.Json.Serialization;
 using UserService.Data;
+using UserService.Repositories.Interfaces;
+using UserService.UnitsOfWork.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 #region Configure database
 
-//builder.Services.AddScoped<IDataConext, DataContext>();
+builder.Services.AddScoped<DataContext>();
 
 var configuration = builder.Configuration;
 
@@ -29,9 +36,35 @@ builder.Services.AddDbContext<DataContext>((serviceProvider, options) =>
     configurator.Configure(options);
 });
 
+builder.Services.AddTransient<SeedDb>();
+
 #endregion Configure database
 
+builder.Services.AddScoped<IUsersRepository, UsersRepository>();
+builder.Services.AddScoped<IUsersUnitOfWork, UsersUnitOfWork>();
+
+builder.Services.AddIdentity<User, IdentityRole>(x =>
+{
+    x.User.RequireUniqueEmail = true;
+    x.Password.RequireDigit = false;
+    x.Password.RequiredUniqueChars = 0;
+    x.Password.RequireLowercase = false;
+    x.Password.RequireNonAlphanumeric = false;
+    x.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<DataContext>()
+    .AddDefaultTokenProviders();
+
 var app = builder.Build();
+SeedData(app);
+
+void SeedData(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using var scope = scopedFactory!.CreateScope();
+    var service = scope.ServiceProvider.GetService<SeedDb>();
+    service!.SeedAsync().Wait();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
---
+++ Minerva/UserService/UnitsOfWork/Interfaces/IUsersUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Entities;
+
+namespace UserService.UnitsOfWork.Interfaces;
+
+public interface IUsersUnitOfWork
+{
+    Task<User> GetUserAsync(string email);
+
+    Task<IdentityResult> AddUserAsync(User user, string password);
+
+    Task CheckRoleAsync(string roleName);
+
+    Task AddUserToRoleAsync(User user, string roleName);
+
+    Task<bool> IsUserInRoleAsync(User user, string roleName);
+}
