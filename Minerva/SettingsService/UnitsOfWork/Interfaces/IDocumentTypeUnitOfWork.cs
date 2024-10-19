8d519202b578528fd992a59b028007eb708b1633
Juan Figueroa <jjfigueroah.desarrollador@gmail.com>
2024-10-19 07:53:56 -0500

    feat: document-type

---
+++ Minerva/SettingsService/UnitsOfWork/Interfaces/IDocumentTypeUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using SharedLibrary.DTOs;
+using SharedLibrary.Entities;
+using SharedLibrary.Responses;
+
+namespace SettingsService.UnitsOfWork.Interfaces;
+
+public interface IDocumentTypeUnitOfWork
+{
+    Task<ActionResponse<DocumentType>> AddAsync(DocumentTypeDTO documentTypeDTO);
+}
